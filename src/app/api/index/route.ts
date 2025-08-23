6b7fa7c9fc3ba2af4cea62c855523d94ecdb1897
hayuya <e3131901@foreign.kitakyu-u.ac.jp>
2025-08-23 21:59:21 +0900

    feat: Add dynamic export declaration to enable forced dynamic rendering

--- src/app/api/index/route.ts
+++ src/app/api/index/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getTerms } from '../../../lib/microcms'
 
-
+export const dynamic = 'force-dynamic'
 
 export async function GET(request: NextRequest) {
   try {
